b59049de413b8d25436fd86e9296ab27356d4c49
Justin Abbott <justin.abbott@tumbleweedtechconsulting.com>
2017-09-28 12:35:37 -0700

    Add abilty card flip mechanic

---
+++ Assets/Cards/Scripts/AbilityCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityCard : MonoBehaviour
+{
+
+	[Tooltip("Select Card Front")] public Sprite CardFront;
+	[Tooltip("Select Card Back")] public Sprite CardBack;
+
+	private Image _image;
+	private bool _flipped;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_image = GetComponent<Image>();
+		_image.sprite = CardFront;
+	}
+	
+	public void FlipCard()
+	{
+		Debug.Log("Flipped " + name);
+		_flipped = !_flipped;
+		_image.sprite = _flipped ? CardBack : CardFront;
+	}
+}
