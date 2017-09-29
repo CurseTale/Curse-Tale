27362268c8e09be86b619903bb93745d010ca157
Justin Abbott <justin.abbott@tumbleweedtechconsulting.com>
2017-09-28 18:11:50 -0700

    Instantiate ability cards from script.

---
+++ Assets/Cards/Scripts/AbilityCards.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCards : MonoBehaviour
+{
+    [Tooltip("Attach four ability cards to insert into scene.")] public List<GameObject> AbiltyCards = new List<GameObject>();
+
+    private int _prefabIndex;
+
+    private void OnValidate()
+    {
+        if (AbiltyCards.Count > 4)
+        {
+            Debug.LogWarning("Do not load more than four cards.");
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnCards();
+        _prefabIndex = 0;
+    }
+
+    /// <summary>
+    /// Spawn ability cards into the scene and parent them to the appropriate object. 
+    /// </summary>
+    private void SpawnCards()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject card = Instantiate(AbiltyCards[_prefabIndex], child.transform.position, Quaternion.identity);
+            card.transform.SetParent(child);
+            _prefabIndex++;
+        }
+    }
+}
---
+++ Assets/Cards/Scripts/CardSlot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSlot : MonoBehaviour {
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(90, 135, 1));
+	}
+}
